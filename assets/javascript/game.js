fbdf00cc6e2fff3bcc44cb29c231fb275fc99032
nnm0122 <nnm0122@gmail.com>
2017-09-16 01:28:50 -0400

    final version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,74 +1,78 @@
+
 // My arrary of Scary Movies. There are 15 items in my Scary Movies Array.
-var wordBank=["Carrie","Ring","Dracula","Insidious","Jigsaw","It","Halloween","Conjuring","Mama","Annabelle","Chucky","Scream","Omen","Saw","Candyman"];
+var movieBank=["Carrie","Ring","Dracula","Insidious","Jigsaw","It","Halloween","Conjuring","Mama","Annabelle","Chucky","Scream","Omen","Saw","Candyman"];
 
-var rand = Math.floor(Math.random() * wordBank.length);
-
-
-var movieChoice = wordBank[rand];
-console.log("computer word " + movieChoice);
+var rand = Math.floor(Math.random() * movieBank.length);
+var movieChoice = movieBank[rand];
 
 var elementWord = document.getElementsByClassName("hangman-word");
 var elementGuessesRemaining = document.getElementsByClassName("text-remain");
 var elementGuessedLetters = document.getElementsByClassName("text-guessed");
+var sound = new Audio("audio_file.mp3");
 
-
+var wins = 0;
+elementWord[0].innerHTML = guessedWord;
+// elementGuessesRemaining[0].innerHTML = guessesRemaining;
+console.log("computer word " + movieChoice);
 var guessedWord = [];
+
+
+// creating underscores of title letters with string.length...
 for (var i = 0; i < movieChoice.length; i++) {
   guessedWord.push("_");
-  //console.log( "guessedWord",guessedWord);
-}   
-
-var wrongGuesses = 6;
-var guessedLetters = [];
-var wins = 0;
-
+}
 
 
 elementWord[0].innerHTML = guessedWord;
-elementGuessesRemaining[0].innerHTML = wrongGuesses;
 
+
+  guessesRemaining = 15;
+    guessedLetters = [];
+    correctGuessedLetters = 0;
+
+
+
+// on keyup event will return an obect with metadata about events, of any kind, but here it's the keyup event.
 document.onkeyup = function(event) {
-
+  {
+    if (guessesRemaining==0){
+  return;
+  var sound = Notification.sound;{
+    sound: "assets/wlaugh.mp3"
+  }
+  notification("GameOver!", elementNotification);
+}
+  }
 
       console.log(event.key);
       if (event.which >= 65 && event.which <= 90) {  //letters are in a keycode and makes sure only a letter is pressed. 65 is =a and 90 is = to z.
         var keyPressed = event.key.toLowerCase();
-        console.log("good!");
-        guessedLetters.push(keyPressed);
-        console.log(guessedLetters);
-            elementGuessedLetters[0].innerHTML = guessedLetters; //displays letters guessed so far
-        wrongGuesses--;
-            elementGuessesRemaining[0].innerHTML = guessesRemaining; //display # of guesses to HTML
-        console.log(guessesRemaining);
 
-        if (guessedLetters.indexOf(keyPressed) !== -1) {  //means it found the letter. If letter has already been guessed it does nothing
-          return;
-        }
+        if (guessedLetters.indexOf(keyPressed) == -1) {  //means if keyPressed is NOT in the guessedLetters array, then...
+          guessedLetters.push(keyPressed); // put the letter in the array.
+          // console.log(guessedLetters);
 
+          guessesRemaining--; // subtract one from guessesRemaining.
+          elementGuessesRemaining[0].innerHTML = guessesRemaining; //display # of guesses remaining to HTML
 
+          elementGuessedLetters[0].innerHTML += keyPressed + ',  '; //displays letters guessed so far
+          for(i = 0; i < movieChoice.length; i++){
 
-        var matchFound = false;
+        if (movieChoice.toLowerCase().charAt(i) == keyPressed) { //need toLowerCase() to find the first letter.
 
-        for (var i = 0; i < movieChoice.length; i++){
-          movieChoice.charAt(i);
-          if (movieChoice.charAt(i) == keyPressed) {
-            guessedWord[i] = keyPressed;
-            matchFound = true;
-          
+                console.log('movieChoice.charAt(i): ',movieChoice.charAt(i));
+                console.log('here is keyPressed: ', keyPressed);
+                guessedWord[i] = movieChoice.charAt(i);
+                elementWord[0].innerHTML = guessedWord;
 
-                console.log("match found? " + matchFound);
+            }
+          }
         }
 }
-        // Updates the HTML
-        //console.log(guessedWord);
-            elementWord[0].innerHTML = guessedWord;
-   
-            console.log("enter if statement")
-            if (movieChoice.indexOf() === guessedWord.indexOf()) {
-                console.log("Correct!")
-            }
-
-      } 
-
- }
-
+}
+// Things I could not figure out.
+//How to get the sound to play once the user won.
+//How to get the # of wins to calculate. I tried using function increments (++ and --) and the following code:
+//if (userGuess === computerGuess) {
+        //  wins++
+  //How to reset the game once the word was guessed. I tried adding a reset button on my HTML page and then.addEventListener function to execute the reset. But it did not work.
